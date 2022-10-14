5efaba29732413267f13c2d930f9d73bc5d07b85
Marcos Roberto <marco.2229roberto@gmail.com>
2022-10-14 12:24:07 -0400

    Inicializa da UC12 -ER02

---
+++ Classes/Pessoa.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using BackEndProj.Interfaces;
+
+
+namespace BackEndProj.Classes
+{
+    public class Pessoa : IPessoa
+    {
+       public string nome { get; set; }
+       public string endereco { get; set; }
+       public float rendimento { get; set; }
+
+        public float PagarImposto(float parRendimento)
+        {
+            throw new NotImplementedException();
+        }
+        //metodo
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using BackEndProj.Classes;
+
+
+PessoaFisica novaPessoaFisica = new PessoaFisica();
+
+novaPessoaFisica.nome = "Marcos";
+novaPessoaFisica.endereco = "Rua bacaba, 328";
+novaPessoaFisica.rendimento = 1000f;
+novaPessoaFisica.cpf = "12345678-00";
+
+Console.WriteLine($"Nome: {novaPessoaFisica.nome}");
+Console.WriteLine($"Endereco: {novaPessoaFisica.endereco}");
+Console.WriteLine($"CPF: {novaPessoaFisica.cpf}");
+Console.WriteLine($"Rendimento: {novaPessoaFisica.rendimento}");
+
